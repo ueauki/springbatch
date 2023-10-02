900d376f8359e30e7ef500a461de85ffa42cf809
ueauki <ueauki@gmail.com>
2023-10-02 19:12:28 +0900

    feature(init): 예제 초기화

---
+++ src/main/java/io/springbatch/springbatchlecture/JobExcutionConfiguration.java
@@ -0,0 +1,56 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobExcutionConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("job")
+                .start(stepFirst())
+                .next(stepSecond())
+                .next(stepThird())
+                .build();
+    }
+
+    @Bean
+    public Step stepFirst() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("================== step 1 ================");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Step stepSecond() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("================== step 2 ================");
+//                        throw new RuntimeException("step 2 failed");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+}
